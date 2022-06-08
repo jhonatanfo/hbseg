39f819c7982dd83bb2b5738054a46cbc6f9da8e1
jhonatan <jnatanfo@hotmail.com>
2022-06-08 01:30:28 -0300

    feat: adicionando pagina contato

--- content/js/padrao.js
+++ content/js/padrao.js
@@ -1,102 +1,36 @@
 $(document).on('ready', function() {
-    var o = $(".princial-banner");
-    $(document).ready(function() {
-        o.slick({
-            arrows: true,
-            dots: true,
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            speed: 500,
-            fade: true,
-            cssEase: 'linear'
-        }), o.slickAnimation()
-    });
 
-    $(".produtos").slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        dots: true,
-        infinite: true,
-        arrows: true,
-        adaptiveHeight: true,
-        responsive: [{
-            breakpoint: 1030,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
-        }, {
-            breakpoint: 767,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
-        }, {
-            breakpoint: 530,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        }]
-    });
 
-    $(".produtos-02").slick({
+    $(".slick-depoimentos").slick({
         infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        dots: true,
-        infinite: true,
-        arrows: true,
-        adaptiveHeight: true,
-        responsive: [{
-            breakpoint: 1030,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
-        }, {
-            breakpoint: 767,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
-        }, {
-            breakpoint: 530,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        }]
-    });
-
-    $(".slide-fotos").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
         dots: false,
         infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
         arrows: true,
+        adaptiveHeight: true,
         responsive: [{
-            breakpoint: 1199,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-            }
-        }, {
-            breakpoint: 991,
+            breakpoint: 1030,
             settings: {
                 slidesToShow: 2,
                 slidesToScroll: 1,
             }
         }, {
-            breakpoint: 540,
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        }, {
+            breakpoint: 530,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
             }
         }]
     });
+
+
 });
 
 $(document).ready(function() {
@@ -119,48 +53,9 @@ $(document).ready(function() {
         }, 500);
     });
 
-    $('.box-produto span, .box-produto-02 span').on('click', function(e) {
-        var id = $(this).attr('class');
-        var url_atual = window.location.href;
-        url = url_atual.split("/");
-        url = url[3];
-        scrolbar(id);
-    });
 
-    function scrolbar(id) {
-        targetOffset = $("#" + id).offset().top;
-        $('html, body').animate({
-            scrollTop: targetOffset - 100
-        }, 500);
-    }
 
-    $(".box-produto-click").click(function() {
-        var item = $(this).attr("id");
-        $(".produto-box").removeClass("ativo");
-        $("." + item).addClass("ativo");
-        AOS.init({
-            easing: 'ease-in-out-sine'
-        });
-    });
 
-    $(".box-produto-click-02").click(function() {
-        var item = $(this).attr("id");
-        $(".produto-box-02").removeClass("ativo");
-        $("." + item).addClass("ativo");
-        AOS.init({
-            easing: 'ease-in-out-sine'
-        });
-    });
-
-    $(".box-produto").hover(function() {
-        $('.box-produto').removeClass("ativo");
-        $(this).addClass("ativo");
-    });
-
-    $(".box-produto-02").hover(function() {
-        $('.box-produto-02').removeClass("ativo");
-        $(this).addClass("ativo");
-    });
 
 
 });
